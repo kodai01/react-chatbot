c284ba5d732608b53777276ed2af39ef8b50321d
kodai <63472444+kodai01@users.noreply.github.com>
2021-02-03 16:37:16 +0900

    4stateの設計を終了

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React, {useState} from 'react'
+import './assets/style.css';
+import defaultDataset from './dataset'
+
+const App = () => {
+
+  return (
+    <section className="c-section">
+      <div className="c-box"></div>
+    </section>
+  );
+}
+
+export default App;
