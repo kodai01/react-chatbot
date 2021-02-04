0a486178b0f68e9602bcf1cb28d6b8f6d2a76f42
kodai <63472444+kodai01@users.noreply.github.com>
2021-02-05 00:33:06 +0900

    7material-uiを用いたデザイン終了

--- src/components/answer.jsx
+++ src/components/answer.jsx
@@ -1,15 +1,25 @@
 import React from 'react';
-// import { makeStyles } from '@material-ui/core/styles';
+import { makeStyles, createStyles } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
-// const useStyles = makeStyles((theme) => ({
-//   root: {
-//   },
-// }));
+const useStyles = makeStyles(() => (
+  createStyles({
+    "button": {
+      border: "1px solid orange",
+      color: "orange",
+      fontWeight: 600,
+      marginBottom: 8,
+      "&:hover": {
+        backgroundColor: "orange",
+        color: "white"
+      }
+    }
+  })
+));
 
 const Answer = (props) => {
-
+  const classes = useStyles()
   return (
-     <Button variant="contained" onClick={() => props.select(props.content, props.nextId)}>{props.content}</Button>
+     <Button className={classes.button} variant="ourlined" onClick={() => props.select(props.content, props.nextId)}>{props.content}</Button>
   )
 }
 
